e92558df99646f2961ce931df582d77bc4754d5d
Jesdanyc <jesdanyc@hotmail.com>
2024-08-14 00:15:29 -0500

    Extencion de la estructura, se añadio mensajes y botones

--- src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
+++ src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
@@ -5,6 +5,10 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.util.Map;
+
+import static es.codegym.telegrambot.TelegramBotContent.*;
+
 public class MyFirstTelegramBot extends MultiSessionTelegramBot {
 
     public static final String NAME = "JdYcMaratonBot_bot";
@@ -20,14 +24,56 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
         // Para negrita : *hola*
         // Para cursiva : _hola_
         // Condicionales
+
         if (getMessageText().equals("/start")){// equals = a =
-            sendTextMessageAsync("_Hola_ *programador* <3");
+            setUserGlory(0);
+            sendTextMessageAsync(STEP_1_TEXT,
+                    Map.of("Hackear la nevera", "step_1_btn"));//Metodo tipo de objeto guarda informacion key and value
         }
-        if (getMessageText().contains("hola")){//h o H son simbolos diferentes and contains igual pero detecta dentro de todo un texto
-            sendTextMessageAsync("_Hola_,como te *llamas?* >.<");
+        if (getCallbackQueryButtonKey().equals("step_1_btn")){
+            addUserGlory(20);
+            sendTextMessageAsync(STEP_2_TEXT,
+                    Map.of("¡Tomar una salchicha! +20 de fama","step_2_btn",
+                            "¡Tomar un pescado! +20 de fama", "step_2_btn",
+                            "¡Tirar una lata de pepinillos +20 de fama","step_2_btn"));
         }
-        if (getMessageText().contains("mi nombre es")){
-            sendTextMessageAsync("Un placer, yo soy *Gato* =^.^=.\n¿En qué te puedo ayudar?");
+        if (getCallbackQueryButtonKey().equals("step_2_btn")){
+            addUserGlory(20);
+            sendTextMessageAsync(STEP_3_TEXT+ getUserGlory(),
+                    Map.of("Hackear al robot aspirador","step_3_btn"));
+        }
+        if (getCallbackQueryButtonKey().equals("step_3_btn")){
+            addUserGlory(30);
+            sendTextMessageAsync(STEP_4_TEXT+ getUserGlory(),
+                    Map.of("Enviar al robot aspirador a por comida! +30 de fama", "step_4_btn",
+                            "Dar un paseo en el robot aspirador! +30 de fama", "step_4_btn",
+                            "¡Huir del robot aspirador! +30 de fama","step_4_btn"));
+        }
+        if (getCallbackQueryButtonKey().equals("step_4_btn")){
+            addUserGlory(30);
+            sendTextMessageAsync(STEP_5_TEXT+ getUserGlory(),
+                    Map.of("Encender y ponerse la GoPro!","step_5_btn"));
+        }
+        if (getCallbackQueryButtonKey().equals("step_5_btn")){
+            addUserGlory(40);
+            sendTextMessageAsync(STEP_6_TEXT+ getUserGlory(),
+                    Map.of("¡Correr por los tejados, grabar con la GoPro! +40 de fama","step_6_btn",
+                            "¡Atacar a otros gatos desde tu escondite con la GoPro! +40 de fama","step_6_btn",
+                            "¡Atacar a los perros desde tu escondite con la GoPro! +40 de fama","step_6_btn"));
+        }
+        if (getCallbackQueryButtonKey().equals("step_6_btn")){
+            addUserGlory(40);
+            sendTextMessageAsync(STEP_7_TEXT+ getUserGlory(),
+                    Map.of("Romper la contraseña","step_7_btn"));
+        }
+        if (getCallbackQueryButtonKey().equals("step_7_btn")){
+            addUserGlory(50);
+            sendTextMessageAsync(STEP_8_TEXT+ getUserGlory(),
+                    Map.of("Salir al patio","step_8_btn"));
+        }
+        if (getCallbackQueryButtonKey().equals("step_8_btn")){
+            addUserGlory(0);
+            sendTextMessageAsync(FINAL_TEXT+ getUserGlory());
         }
     }
 
